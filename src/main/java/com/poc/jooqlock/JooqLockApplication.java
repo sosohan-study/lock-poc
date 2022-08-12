8adab0e1004e98bdd0d230e696152c15c528e9fa
kwj1270 <kwj1270@naver.com>
2022-08-12 19:47:36 +0900

    init(kwj1270): initialize project (#1)

---
+++ src/main/java/com/poc/jooqlock/JooqLockApplication.java
@@ -0,0 +1,13 @@
+package com.poc.jooqlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JooqLockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poc/jooqlock/JooqLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.jooqlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooqLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
